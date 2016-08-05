7d054aff453e6f8bc87ba0a8b25ae3c335777b19
erinsul <erin_sullivan50@yahoo.com>
2016-08-05 14:33:20 -0400

    Add code to validate uniqueness of place/profile in trip model
    Added code to validate place and profile uniqueness in trip model so the
    same trip can't be added twice

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,7 @@ class Trip < ActiveRecord::Base
   belongs_to :place, inverse_of: :trips
   has_many :descriptions
   has_many :tags, through: :descriptions
+
+  validates :profile_id, uniqueness: {scope: :place_id}
+  validates :place_id, uniqueness: {scope: :profile_id}
 end
