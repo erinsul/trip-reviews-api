8613c5757b9a66541088ee01614ce6062f2d0ec9
erinsul <erin_sullivan50@yahoo.com>
2016-08-16 13:43:22 -0400

    Edit routes
    Edited routes to include only those used by front end at the moment

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :trips, except: [:new, :edit]
-  resources :descriptions, except: [:new, :edit]
+  resources :descriptions, only: [:index, :show, :create, :delete]
   resources :places, only: [:index, :show, :create]
   resources :tags, only: [:index]
-  resources :profiles, except: [:new, :edit]
-  resources :examples, except: [:new, :edit]
+  resources :profiles, only: [:index, :create, :update]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
